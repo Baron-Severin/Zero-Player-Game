198e044cb6ead1ed567f87a62505c26661b3e5b8
Baron-Severin <severinrudie@gmail.com>
2016-04-22 13:30:09 -0700

    updated how PositionObjects function, cleaned up and partially commented roadmap

--- src/zeroPlayerGamePackage/PositionObject.java
+++ src/zeroPlayerGamePackage/PositionObject.java
@@ -8,21 +8,35 @@ public class PositionObject {
 		
 //		System.out.println(positionX);
 //		System.out.println(positionY);
-	}
+	};
 
 	private int positionX;
 	private int positionY;
 	
 	public int getPositionX() {
 		return positionX;
-	}
+	};
 	
 	public int getPositionY() {
 		return positionY;
 		
+	};
+	
+	public void setPositionX (int position) {
+		positionX = position;
+	}
+	
+	public void setPositionY (int position) {
+		positionY = position;
+	}
+	
+	public void setPosition (PositionObject position) {
+		positionX = position.getPositionX();
+		positionY = position.getPositionY();
 	}
 	
 	public String getPositionString() {
 		return (this.getPositionX() + ", " + this.getPositionY());
 	}
-}
+	
+}  // end PositionObject
